ba93f6887392c8bf7d859c254ecbbbcf5d2f69e2
Makar Batura <batura.makar@gmail.com>
2025-10-13 17:03:08 +0300

    + 1

--- java/task/Main.java
+++ java/task/Main.java
@@ -6,16 +6,16 @@ public class Main {
     public static void main(String[] args)
     {
         ArrayList<Student> list1 = new ArrayList<>();
-        list1.add(new Student(1, "Иван", 101, 4.5));
-        list1.add(new Student(3, "Мария", 101, 4.8));
-        list1.add(new Student(5, "Петр", 102, 3.9));
-        list1.add(new Student(7, "Анна", 102, 4.2));
+        list1.add(new Student(38529417, "Иван", 5, 4.5));
+        list1.add(new Student(72015683, "Мария", 5, 9.8));
+        list1.add(new Student(49163825, "Петр", 6, 7.8));
+        list1.add(new Student(86401295, "Анна", 6, 9.2));
         
         ArrayList<Student> list2 = new ArrayList<>();
-        list2.add(new Student(2, "Сергей", 101, 4.1));
-        list2.add(new Student(3, "Мария", 101, 4.8));
-        list2.add(new Student(6, "Ольга", 103, 4.7));
-        list2.add(new Student(7, "Анна", 102, 4.2));
+        list2.add(new Student(23756940, "Сергей", 5, 6.1));
+        list2.add(new Student(72015683, "Мария", 5, 9.8));
+        list2.add(new Student(61587342, "Ольга", 7, 8.7));
+        list2.add(new Student(86401295, "Анна", 6, 9.2));
         
         Students.Sort(list1);
         Students.Sort(list2);
@@ -26,24 +26,21 @@ public class Main {
         System.out.println("\nВторой список студентов:");
         printStudents(list2);
         
-        ArrayList<Student> union = Students.unification(list1, list2);
         System.out.println("\nОбъединение списков:");
-        printStudents(union);
+        printStudents(Students.unification(list1, list2));
 
-        ArrayList<Student> intersect = Students.intersection(list1, list2);
         System.out.println("\nПересечение списков:");
-        printStudents(intersect);
+        printStudents(Students.intersection(list1, list2));
         
-        
-        ArrayList<Student> diff = Students.difference(list1, list2);
-        System.out.println("\nРазность (list1 - list2):");
-        printStudents(diff);
+        System.out.println("\nРазность:");
+        printStudents(Students.difference(list1, list2));
     }
 
     private static void printStudents(ArrayList<Student> students) 
     {
         for (Student student : students) {
-            System.out.println(student);
+            System.out.println("№ " + student.m_ID + ", " + student.m_name + ", " 
+                               + student.m_group + " группа, " + student.m_grade);
         }
     }
 }
